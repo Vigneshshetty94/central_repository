57a0b28980675110a11e1ca9db44f7e88545fd4e
Vigneshshetty94 <90980725+Vigneshshetty94@users.noreply.github.com>
2021-09-26 13:46:46 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -7,6 +7,6 @@ factorial()
     for(i=1;i<=number;i++){    
       fact=fact*i;    
   }    
-  printf("factorial of %d is: %d",number,fact);    
+  printf("fact of %d is: %d",number,fact);    
 // return 0;  
 } 
